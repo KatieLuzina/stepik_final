0f2c39a664aac9a1475afc51a2d7774270844928
Екатерина Пага <ekaterina.paga@playrix.com>
2024-07-16 18:37:11 +0500

    add steps 5, 6 from lesson 4.2

---
+++ pages/main_page.py
@@ -0,0 +1,10 @@
+from .base_page import BasePage
+
+
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(by='id', value="login_link")
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.browser.find_element(by='id', value="login_link"), "Login link is not presented"
